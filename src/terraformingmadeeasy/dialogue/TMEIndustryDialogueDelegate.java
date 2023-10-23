135440fed2cced79310d8b5ee5b552f343455c9a
AudaxLudos <james.tarrobal.777@gmail.com>
2023-10-23 16:08:48 +0800

    Update UI implementation

--- src/terraformingmadeeasy/dialogue/TMEIndustryDialogueDelegate.java
+++ src/terraformingmadeeasy/dialogue/TMEIndustryDialogueDelegate.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class TMEIndustryDialogueDelegate implements CustomDialogDelegate {
     public static final float WIDTH = 600f;
 
-    public static final float HEIGHT = 600f;
+    public static final float HEIGHT = 400f;
 
     public TMEBaseIndustry industry;
 
@@ -34,40 +34,91 @@ public class TMEIndustryDialogueDelegate implements CustomDialogDelegate {
 
     @Override
     public void createCustomDialog(CustomPanelAPI panel, CustomDialogCallback callback) {
-        float oPad = 10f;
-        float sPad = 2f;
         this.buttons.clear();
+        Color baseColor = Misc.getButtonTextColor();
+        Color bgColour = Misc.getDarkPlayerColor();
+        Color brightColor = Misc.getBrightPlayerColor();
 
         TooltipMakerAPI panelTooltip = panel.createUIElement(WIDTH, HEIGHT, true);
         panelTooltip.addSectionHeading("Select planet condition to terraform", Alignment.MID, 0f);
 
-        for (TMEBaseIndustry.ModifiableCondition modifiableCondition : this.industry.modifiableConditions) {
-            int buildTime = Math.round(modifiableCondition.buildTime);
+        for (final TMEBaseIndustry.ModifiableCondition modifiableCondition : this.industry.modifiableConditions) {
             float cost = modifiableCondition.cost;
-            boolean canAfford = (Global.getSector().getPlayerFleet().getCargo().getCredits().get() >= cost);
+            int buildTime = Math.round(modifiableCondition.buildTime);
+            boolean canAfford = Global.getSector().getPlayerFleet().getCargo().getCredits().get() >= cost;
             boolean canBuild = this.industry.canTerraformCondition(modifiableCondition);
             boolean hasCondition = this.industry.getMarket().hasCondition(modifiableCondition.spec.getId());
-            Color baseColor = Misc.getButtonTextColor();
-            Color bgColour = Misc.getDarkPlayerColor();
-            Color brightColor = Misc.getBrightPlayerColor();
             if (!canAfford) {
                 baseColor = Color.darkGray;
                 bgColour = Color.lightGray;
                 brightColor = Color.gray;
             }
-            CustomPanelAPI conditionButtonPanel = panel.createCustomPanel(WIDTH, 40f, new ButtonReportingCustomPanel(this));
-            TooltipMakerAPI buttonElement = conditionButtonPanel.createUIElement(WIDTH, 40f, false);
-            buttonElement.getPosition().inTL(-10f, 0f);
 
-            String textAddOrRemove = hasCondition ? "Remove" : "Add";
-            ButtonAPI areaCheckbox = buttonElement.addAreaCheckbox(textAddOrRemove + " " + modifiableCondition.spec.getName(), modifiableCondition.spec.getId(), baseColor, bgColour, brightColor, WIDTH, 40f, 0f, true);
+            // Clickable button for condition
+            CustomPanelAPI conditionButtonPanel = panel.createCustomPanel(WIDTH, 40f, new ButtonReportingCustomPanel(this));
+            String addOrRemoveText = hasCondition ? "Remove " : "Add ";
+            TooltipMakerAPI buttonElement2 = conditionButtonPanel.createUIElement(WIDTH, 40f, false);
+            buttonElement2.beginGrid(WIDTH / 6f, 3);
+            buttonElement2.addToGrid(0, 0, addOrRemoveText + modifiableCondition.spec.getName(), "");
+            buttonElement2.addToGrid(2, 0, "", buildTime + "", Misc.getHighlightColor());
+            buttonElement2.addToGrid(4, 0, "", Misc.getDGSCredits(cost), canAfford ? Misc.getHighlightColor() : Misc.getNegativeHighlightColor());
+            buttonElement2.addGrid(12f);
+
+            TooltipMakerAPI anchor = conditionButtonPanel.createUIElement(WIDTH, 40f, false);
+            ButtonAPI areaCheckbox = anchor.addAreaCheckbox("", modifiableCondition.spec.getId(),
+                    baseColor, bgColour, brightColor, WIDTH - 5f, 40f, 0f);
             areaCheckbox.setChecked((this.selected == modifiableCondition));
             areaCheckbox.setEnabled((canAfford && canBuild));
-            conditionButtonPanel.addUIElement(buttonElement);
+
+            // Add tooltip to modifiable condition
+            TooltipMakerAPI.TooltipCreator conditionTooltipCreator = new TooltipMakerAPI.TooltipCreator() {
+                @Override
+                public boolean isTooltipExpandable(Object tooltipParam) {
+                    return false;
+                }
+
+                @Override
+                public float getTooltipWidth(Object tooltipParam) {
+                    return 380f;
+                }
+
+                @Override
+                public void createTooltip(TooltipMakerAPI tooltip, boolean expanded, Object tooltipParam) {
+                    if (!modifiableCondition.restrictions.isEmpty())
+                        tooltip.addPara(
+                                "Restrictions: %s", 0f, Misc.getNegativeHighlightColor(),
+                                modifiableCondition.restrictions.toString()
+                        );
+                    else
+                        tooltip.addPara("Restrictions: %s", 0f, Misc.getHighlightColor(), "No restrictions found");
+                    tooltip.addSpacer(10f);
+                    if (!modifiableCondition.requirements.isEmpty())
+                        tooltip.addPara(
+                                "Requirements: %s", 0f, Misc.getHighlightColor(),
+                                modifiableCondition.requirements.toString()
+                        );
+                    else
+                        tooltip.addPara("Requirements: %s", 0f, Misc.getHighlightColor(), "No requirements found");
+                }
+            };
+            buttonElement2.addTooltipTo(conditionTooltipCreator, conditionButtonPanel, TooltipMakerAPI.TooltipLocation.RIGHT);
+
+            // UI elements go over each other
+            conditionButtonPanel.addUIElement(anchor).inTL(-10f, 0f);
+            conditionButtonPanel.addUIElement(buttonElement2).inTL(0f, 0f);
             panelTooltip.addCustom(conditionButtonPanel, 0f);
             this.buttons.add(areaCheckbox);
         }
-        panel.addUIElement(panelTooltip).inTL(0f, 0f);
+        panel.addUIElement(panelTooltip);
+
+        // Show player credits
+        CustomPanelAPI creditsPanel = panel.createCustomPanel(WIDTH, 0f, null);
+        TooltipMakerAPI creditsElement = creditsPanel.createUIElement(WIDTH, 0f, false);
+        creditsElement.setParaSmallInsignia();
+        creditsElement.addPara("Credits: %s", 0f, Misc.getGrayColor(), Misc.getHighlightColor(),
+                Misc.getWithDGS(Global.getSector().getPlayerFleet().getCargo().getCredits().get()));
+        creditsPanel.addUIElement(creditsElement);
+        panel.addComponent(creditsPanel).inBR(0f, -30f);
     }
 
     @Override
