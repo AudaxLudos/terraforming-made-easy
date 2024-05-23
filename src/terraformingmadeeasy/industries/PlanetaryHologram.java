65f23a47faf25674fb1c48692cd75b1fc628fba6
AudaxLudos <james.tarrobal.777@gmail.com>
2024-05-24 01:43:07 +0800

    Update planetary hologram tooltip ui Update planetary hologram cost multiplier to 20 from 100 Update planetary hologram build time to 60 from 360 Update planetary hologram upkeep to 2 from 15 Update planetary hologram implementation

--- src/terraformingmadeeasy/industries/PlanetaryHologram.java
+++ src/terraformingmadeeasy/industries/PlanetaryHologram.java
@@ -2,14 +2,23 @@ package terraformingmadeeasy.industries;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.PlanetSpecAPI;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.impl.campaign.procgen.PlanetGenDataSpec;
 import com.fs.starfarer.api.impl.campaign.procgen.StarSystemGenerator;
+import com.fs.starfarer.api.ui.Alignment;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
 import com.fs.starfarer.loading.specs.PlanetSpec;
 import terraformingmadeeasy.Utils;
 
 import java.util.Objects;
 
 public class PlanetaryHologram extends TMEBaseIndustry {
+    String originalPlanetType = null;
+    String originalPlanetName = null;
+    boolean originalIsGasGiant = false;
+    String fakePlanetType = null;
+
     public PlanetaryHologram() {
         for (PlanetGenDataSpec pDataSpec : Global.getSettings().getAllSpecs(PlanetGenDataSpec.class)) {
             for (PlanetSpecAPI pSpec : Global.getSettings().getAllPlanetSpecs()) {
@@ -18,8 +27,8 @@ public class PlanetaryHologram extends TMEBaseIndustry {
                             pDataSpec.getId(),
                             pSpec.getName(),
                             pSpec.getTexture(),
-                            200000f,
-                            60f,
+                            100000f,
+                            30f,
                             true,
                             null,
                             null,
@@ -34,27 +43,57 @@ public class PlanetaryHologram extends TMEBaseIndustry {
     }
 
     @Override
+    public void advance(float amount) {
+        super.advance(amount);
+
+        if (this.fakePlanetType != null && !Objects.equals(this.market.getPlanetEntity().getTypeId(), this.fakePlanetType)) {
+            changePlanetVisuals(this.fakePlanetType);
+        }
+    }
+
+    @Override
+    public void notifyBeingRemoved(MarketAPI.MarketInteractionMode mode, boolean forUpgrade) {
+        super.notifyBeingRemoved(mode, forUpgrade);
+        if (this.originalPlanetType != null) {
+            changePlanetVisuals(this.originalPlanetType);
+        }
+    }
+
+    @Override
+    protected void addPostUpkeepSection(TooltipMakerAPI tooltip, IndustryTooltipMode mode) {
+        float oPad = 10f;
+        float pad = 3f;
+
+        if (mode == IndustryTooltipMode.NORMAL) {
+            tooltip.addSpacer(10f);
+            tooltip.addSectionHeading("Projected Hologram", Alignment.MID, 0f);
+            tooltip.addPara("Original Planet Type: %s", oPad, Misc.getHighlightColor(), this.market.getPlanetEntity().getTypeId());
+            tooltip.addPara("Fake Planet Type: %s", pad, Misc.getHighlightColor(), this.fakePlanetType != null ? this.fakePlanetType : "-");
+        }
+    }
+
+    @Override
     public void changePlanetVisuals(String planetTypeId) {
-        String prevPlanetType = this.market.getPlanetEntity().getSpec().getPlanetType();
-        String prevPlanetName = this.market.getPlanetEntity().getSpec().getName();
-        boolean prevIsGasGiant = this.market.getPlanetEntity().getSpec().isGasGiant();
-        String newPlanetType = planetTypeId;
-        if (this.modifiableCondition.planetSpecOverride != null) {
+        this.originalPlanetType = this.market.getPlanetEntity().getSpec().getPlanetType();
+        this.originalPlanetName = this.market.getPlanetEntity().getSpec().getName();
+        this.originalIsGasGiant = this.market.getPlanetEntity().getSpec().isGasGiant();
+        this.fakePlanetType = planetTypeId;
+        if (this.modifiableCondition != null && this.modifiableCondition.planetSpecOverride != null) {
             for (PlanetSpecAPI spec : Global.getSettings().getAllPlanetSpecs()) {
                 if (spec.isStar()) {
                     continue;
                 }
                 if (Objects.equals(spec.getPlanetType(), this.modifiableCondition.planetSpecOverride)) {
-                    newPlanetType = spec.getPlanetType();
+                    this.fakePlanetType = spec.getPlanetType();
                     break;
                 }
             }
         }
         // Change the planet type but make sure certain info are still its based info
-        this.market.getPlanetEntity().changeType(newPlanetType, StarSystemGenerator.random);
-        ((PlanetSpec) this.market.getPlanetEntity().getSpec()).planetType = prevPlanetType;
-        ((PlanetSpec) this.market.getPlanetEntity().getSpec()).name = prevPlanetName;
-        ((PlanetSpec) this.market.getPlanetEntity().getSpec()).setGasGiant(prevIsGasGiant);
+        this.market.getPlanetEntity().changeType(this.fakePlanetType, StarSystemGenerator.random);
+        ((PlanetSpec) this.market.getPlanetEntity().getSpec()).planetType = this.originalPlanetType;
+        ((PlanetSpec) this.market.getPlanetEntity().getSpec()).name = this.originalPlanetName;
+        ((PlanetSpec) this.market.getPlanetEntity().getSpec()).setGasGiant(this.originalIsGasGiant);
         this.market.getPlanetEntity().applySpecChanges();
     }
 }
