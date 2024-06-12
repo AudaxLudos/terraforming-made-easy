ad45adc78cf8607f2ad9d604e5953136c9c280f3
AudaxLudos <james.tarrobal.777@gmail.com>
2024-06-12 23:34:02 +0800

    Update industries descriptions

--- src/terraformingmadeeasy/industries/UnificationCenter.java
+++ src/terraformingmadeeasy/industries/UnificationCenter.java
@@ -1,11 +1,18 @@
 package terraformingmadeeasy.industries;
 
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import terraformingmadeeasy.Utils;
+import terraformingmadeeasy.conditions.DeathWorld;
 import terraformingmadeeasy.ids.TMEIds;
 
 public class UnificationCenter extends TMEBaseIndustry {
     public UnificationCenter() {
         getTerraformingOptions(TMEIds.UNIFICATION_CENTER);
+    }
+
+    @Override
+    protected void addRightAfterDescriptionSection(TooltipMakerAPI tooltip, IndustryTooltipMode mode) {
+        super.addRightAfterDescriptionSection(tooltip, mode);
     }
 
     @Override
