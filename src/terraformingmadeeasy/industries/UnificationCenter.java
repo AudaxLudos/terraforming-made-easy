c5eb8f464125d2ab922912e2b3d92dec113253c6
AudaxLudos <james.tarrobal.777@gmail.com>
2024-06-14 00:14:13 +0800

    Add unification center AI core reduced demand of 1 effect

--- src/terraformingmadeeasy/industries/UnificationCenter.java
+++ src/terraformingmadeeasy/industries/UnificationCenter.java
@@ -1,10 +1,15 @@
 package terraformingmadeeasy.industries;
 
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.econ.CommoditySpecAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
 import terraformingmadeeasy.Utils;
 import terraformingmadeeasy.conditions.DeathWorld;
 import terraformingmadeeasy.ids.TMEIds;
+
+import java.awt.*;
 
 public class UnificationCenter extends TMEBaseIndustry {
     public UnificationCenter() {
@@ -29,8 +34,69 @@ public class UnificationCenter extends TMEBaseIndustry {
     }
 
     @Override
-    protected void addRightAfterDescriptionSection(TooltipMakerAPI tooltip, IndustryTooltipMode mode) {
-        super.addRightAfterDescriptionSection(tooltip, mode);
+    protected void addAlphaCoreDescription(TooltipMakerAPI tooltip, AICoreDescriptionMode mode) {
+        float oPad = 10f;
+        Color highlight = Misc.getHighlightColor();
+
+        String pre = "Alpha-level AI core currently assigned. ";
+        if (mode == AICoreDescriptionMode.MANAGE_CORE_DIALOG_LIST || mode == AICoreDescriptionMode.INDUSTRY_TOOLTIP) {
+            pre = "Alpha-level AI core. ";
+        }
+        if (mode == AICoreDescriptionMode.INDUSTRY_TOOLTIP || mode == AICoreDescriptionMode.MANAGE_CORE_TOOLTIP) {
+            CommoditySpecAPI coreSpec = Global.getSettings().getCommoditySpec(this.aiCoreId);
+            TooltipMakerAPI text = tooltip.beginImageWithText(coreSpec.getIconName(), 48);
+            text.addPara(pre + "Reduces upkeep cost by %s. Reduces demand by %s unit. Reduces terraforming time by %s.", oPad, highlight,
+                    (int) ((1f - UPKEEP_MULT) * 100f) + "%", "" + DEMAND_REDUCTION, (int) (ALPHA_BUILD_TIME_MULT * 100f) + "%");
+            tooltip.addImageWithText(oPad);
+            return;
+        }
+
+        tooltip.addPara(pre + "Reduces upkeep cost by %s. Reduces demand by %s unit. Reduces terraforming time by %s.", oPad, highlight,
+                (int) ((1f - UPKEEP_MULT) * 100f) + "%", "" + DEMAND_REDUCTION, (int) (ALPHA_BUILD_TIME_MULT * 100f) + "%");
+    }
+
+    @Override
+    protected void addBetaCoreDescription(TooltipMakerAPI tooltip, AICoreDescriptionMode mode) {
+        float oPad = 10f;
+        Color highlight = Misc.getHighlightColor();
+
+        String pre = "Beta-level AI core currently assigned. ";
+        if (mode == AICoreDescriptionMode.MANAGE_CORE_DIALOG_LIST || mode == AICoreDescriptionMode.INDUSTRY_TOOLTIP) {
+            pre = "Beta-level AI core. ";
+        }
+        if (mode == AICoreDescriptionMode.INDUSTRY_TOOLTIP || mode == AICoreDescriptionMode.MANAGE_CORE_TOOLTIP) {
+            CommoditySpecAPI coreSpec = Global.getSettings().getCommoditySpec(this.aiCoreId);
+            TooltipMakerAPI text = tooltip.beginImageWithText(coreSpec.getIconName(), 48);
+            text.addPara(pre + "Reduces upkeep cost by %s. Reduces demand by %s unit. Reduces terraforming time by %s.", oPad, highlight,
+                    (int) ((1f - UPKEEP_MULT) * 100f) + "%", "" + DEMAND_REDUCTION, (int) (BETA_BUILD_TIME_MULT * 100f) + "%");
+            tooltip.addImageWithText(oPad);
+            return;
+        }
+
+        tooltip.addPara(pre + "Reduces upkeep cost by %s. Reduces demand by %s unit. Reduces terraforming time by %s.", oPad, highlight,
+                (int) ((1f - UPKEEP_MULT) * 100f) + "%", "" + DEMAND_REDUCTION, (int) (BETA_BUILD_TIME_MULT * 100f) + "%");
+    }
+
+    @Override
+    protected void addGammaCoreDescription(TooltipMakerAPI tooltip, AICoreDescriptionMode mode) {
+        float oPad = 10f;
+        Color highlight = Misc.getHighlightColor();
+
+        String pre = "Gamma-level AI core currently assigned. ";
+        if (mode == AICoreDescriptionMode.MANAGE_CORE_DIALOG_LIST || mode == AICoreDescriptionMode.INDUSTRY_TOOLTIP) {
+            pre = "Gamma-level AI core. ";
+        }
+        if (mode == AICoreDescriptionMode.INDUSTRY_TOOLTIP || mode == AICoreDescriptionMode.MANAGE_CORE_TOOLTIP) {
+            CommoditySpecAPI coreSpec = Global.getSettings().getCommoditySpec(this.aiCoreId);
+            TooltipMakerAPI text = tooltip.beginImageWithText(coreSpec.getIconName(), 48);
+            text.addPara(pre + "Reduces upkeep cost by %s. Reduces demand by %s unit. Reduces terraforming time by %s.", oPad, highlight,
+                    (int) ((1f - UPKEEP_MULT) * 100f) + "%", "" + DEMAND_REDUCTION, (int) (GAMMA_BUILD_TIME_MULT * 100f) + "%");
+            tooltip.addImageWithText(oPad);
+            return;
+        }
+
+        tooltip.addPara(pre + "Reduces upkeep cost by %s. Reduces demand by %s unit. Reduces terraforming time by %s.", oPad, highlight,
+                (int) ((1f - UPKEEP_MULT) * 100f) + "%", "" + DEMAND_REDUCTION, (int) (GAMMA_BUILD_TIME_MULT * 100f) + "%");
     }
 
     @Override
