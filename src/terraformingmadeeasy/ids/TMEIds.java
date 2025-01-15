a81f01aad04839071265c5362593d54c10e7fb49
AudaxLudos <james.tarrobal.777@gmail.com>
2025-01-15 16:33:10 +0800

    Update research tech ids for AotD-VoK

--- src/terraformingmadeeasy/ids/TMEIds.java
+++ src/terraformingmadeeasy/ids/TMEIds.java
@@ -19,17 +19,17 @@ public class TMEIds {
     public static final String TERRESTRIAL_ENGINE = "tme_terrestrial_engine";
     public static final String UNIFICATION_CENTER = "tme_unification_center";
 
-    // TME AOTD Ids
-    public static final String AGRICULTURAL_LABORATORY_TECH = "tme_tech_agricultural_laboratory";
-    public static final String ATMOSPHERE_REGULATOR_TECH = "tme_tech_atmosphere_regulator";
-    public static final String CONSTRUCTION_GRID_TECH = "tme_tech_construction_grid";
-    public static final String ELEMENT_SYNTHESIZER_TECH = "tme_tech_element_synthesizer";
-    public static final String GEOMORPHOLOGY_STATION_TECH = "tme_tech_geomorphology_station";
-    public static final String MINERAL_REPLICATOR_TECH = "tme_tech_mineral_replicator";
-    public static final String PLANETARY_HOLOGRAM_TECH = "tme_tech_planetary_hologram";
-    public static final String STELLAR_MANUFACTORY_TECH = "tme_tech_stellar_manufactory";
-    public static final String TERRESTRIAL_ENGINE_TECH = "tme_tech_terrestrial_engine";
-    public static final String UNIFICATION_CENTER_TECH = "tme_tech_unification_center";
+    // TME AoTD-VoK Ids
+    public static final String AGRICULTURAL_LABORATORY_TECH = "tme_tech_agricultural_enrichment";
+    public static final String ATMOSPHERE_REGULATOR_TECH = "tme_tech_atmospheric_regulation";
+    public static final String CONSTRUCTION_GRID_TECH = "tme_tech_megastructure_engineering";
+    public static final String ELEMENT_SYNTHESIZER_TECH = "tme_tech_element_nucleosynthesis";
+    public static final String GEOMORPHOLOGY_STATION_TECH = "tme_tech_surface_restructuring";
+    public static final String MINERAL_REPLICATOR_TECH = "tme_tech_mineral_transmutation";
+    public static final String PLANETARY_HOLOGRAM_TECH = "tme_tech_stellar_topography";
+    public static final String STELLAR_MANUFACTORY_TECH = "tme_tech_climate_reformation";
+    public static final String TERRESTRIAL_ENGINE_TECH = "tme_tech_core_manipulation";
+    public static final String UNIFICATION_CENTER_TECH = "tme_tech_global_centralization";
 
     // Custom entities
     public static final String TME_STATION = "tme_station";
